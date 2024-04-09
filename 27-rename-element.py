c854194cf76e904872f55f375b829c58a88af252
Xizhu Hou <xhou@iriusrisk.com>
2024-04-09 16:27:59 +1000

    leetcode,simple,27-rename-element.py

---
+++ 27-rename-element.py
@@ -0,0 +1,84 @@
+'''
+27. Remove Element
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100
+'''
+
+from typing import List
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int], val: int) -> int:
+        ll = len(nums)
+        ii = 0
+        while ii < len(nums):
+            if nums[ii] == val:
+                nums.pop(ii)
+            else:
+                ii += 1
+
+        return ll-len(nums)
+
+if __name__ == "__main__":
+    app = Solution()
+    test1 = [0,1,2,2,3,0,4,2]
+    val = 2
+    print(app.leetcode(test1, val))
+    print(test1)
+
+# the test case #2 is incorrect, [0,1,2,2,3,0,4,2], val = 2
+# with return 3 and output of [0, 1, 3, 0, 4] is correct but leetcode expects answer is [0, 1, 4, 0, 3]
