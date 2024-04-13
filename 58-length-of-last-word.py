f8fbbf10802efc59122c6d82a2084f00ff948102
Xizhu Hou <xhou@iriusrisk.com>
2024-04-14 00:56:10 +1000

    leetcode,simple,58-length-of-last-word.py

---
+++ 58-length-of-last-word.py
@@ -0,0 +1,73 @@
+'''
+58. Length of Last Word
+Solved
+Easy
+Topics
+Companies
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only English letters and spaces ' '.
+There will be at least one word in s.
+'''
+
+from typing import List
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        ll = len(s)
+        if ll == 0:
+            return 0
+        start = end = 0
+        for ii in range(ll-1,-1,-1):
+            print(start,end)
+            if end == 0:
+                if s[ii] == ' ':
+                    continue
+                else:
+                    end = ii
+            elif s[ii] == ' ':
+                start = ii+1
+                break
+        print(start,end)
+
+        return end-start+1
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [1,3,5,6]
+    a = " a"
+    b = 5
+    b = 2
+    print(app.leetcode(a))
