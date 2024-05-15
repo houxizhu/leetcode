c43e45107afb6d8a66b6fe08cfff5217f99903be
Xizhu Hou <houxizhu@gmail.com>
2024-05-15 14:09:42 +1000

    leetcode,easy, 191-numer-of-1-bits.py

---
+++ 191-numer-of-1-bits.py
@@ -0,0 +1,79 @@
+'''
+
+Code
+Testcase
+Test Result
+Test Result
+191. Number of 1 Bits
+Solved
+Easy
+Topics
+Companies
+Write a function that takes the binary representation of a positive integer and returns the number of 
+set bits
+ it has (also known as the Hamming weight).
+
+ 
+
+Example 1:
+
+Input: n = 11
+
+Output: 3
+
+Explanation:
+
+The input binary string 1011 has a total of three set bits.
+
+Example 2:
+
+Input: n = 128
+
+Output: 1
+
+Explanation:
+
+The input binary string 10000000 has a total of one set bit.
+
+Example 3:
+
+Input: n = 2147483645
+
+Output: 30
+
+Explanation:
+
+The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+ 
+
+Follow up: If this function is called many times, how would you optimize it?
+'''
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        result = 0
+        for ii in range(32):
+            if n & (1<<ii):
+                result += 1
+        return result
+
+if __name__ == "__main__":
+    app = Solution()
+    a = 'AAA'
+    print(app.leetcode(a))
