22aab4144b58ce93810a76d98fa7e43324a013db
Xizhu Hou <houxizhu@gmail.com>
2024-05-29 11:52:47 +1000

    leetcode,daily-challenge,20240529-1404-number-of-steps-to-reduce-a-number-binary-to1.py

---
+++ daily-challenge/20240529-1404-number-of-steps-to-reduce-a-number-binary-to1.py
@@ -0,0 +1,77 @@
+"""
+1404. Number of Steps to Reduce a Number in Binary Representation to One
+Solved
+Medium
+Topics
+Companies
+Hint
+Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+
+If the current number is even, you have to divide it by 2.
+
+If the current number is odd, you have to add 1 to it.
+
+It is guaranteed that you can always reach one for all test cases.
+
+ 
+
+Example 1:
+
+Input: s = "1101"
+Output: 6
+Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14. 
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.  
+Step 5) 4 is even, divide by 2 and obtain 2. 
+Step 6) 2 is even, divide by 2 and obtain 1.  
+Example 2:
+
+Input: s = "10"
+Output: 1
+Explanation: "10" corressponds to number 2 in their decimal representation.
+Step 1) 2 is even, divide by 2 and obtain 1.  
+Example 3:
+
+Input: s = "1"
+Output: 0
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of characters '0' or '1'
+s[0] == '1'
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        count = 0
+        istr = 0
+        for each in s:
+            istr = istr*2+int(each)
+        #print(istr,s)
+        while istr > 1:
+            count += 1
+            if istr & 1:
+                istr += 1
+            else:
+                istr >>= 1
+
+        return count
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [0,1,2,4,5,7]
+    print(app.leetcode(a))
