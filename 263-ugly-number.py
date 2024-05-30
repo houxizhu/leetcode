a7333b565267fb531971ac1c17f74bcede8c5855
Xizhu Hou <houxizhu@gmail.com>
2024-05-31 00:29:36 +1000

    leetcode,easy,263-ugly-number.py

---
+++ 263-ugly-number.py
@@ -0,0 +1,64 @@
+"""
+263. Ugly Number
+Solved
+Easy
+Topics
+Companies
+An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+Given an integer n, return true if n is an ugly number.
+
+ 
+
+Example 1:
+
+Input: n = 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: n = 1
+Output: true
+Explanation: 1 has no prime factors, therefore all of its prime factors are limited to 2, 3, and 5.
+Example 3:
+
+Input: n = 14
+Output: false
+Explanation: 14 is not ugly since it includes the prime factor 7.
+ 
+
+Constraints:
+
+-231 <= n <= 231 - 1
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> bool:
+        while n > 1:
+            if n % 2 == 0:
+                n //= 2
+            elif n % 3 == 0:
+                n //= 3
+            elif n % 5 == 0:
+                n //= 5
+            else:
+                return False
+        if n == 1:
+            return True
+
+        return False
+
+if __name__ == "__main__":
+    app = Solution()
+    a = 38
+    print(app.leetcode(a))
