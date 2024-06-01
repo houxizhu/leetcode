ad5bd35bb53c310862cda57cac39b6665c0f1dae
Xizhu Hou <houxizhu@gmail.com>
2024-06-01 16:04:27 +1000

    leetcode,daily-challenge,2024jun,01-3110-score-of-a-string.py

---
+++ daily-challenge/jun/01-3110-score-of-a-string.py
@@ -0,0 +1,62 @@
+"""
+3110. Score of a String
+Easy
+Topics
+Companies
+Hint
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        ll = len(s)
+        result = 0
+        for ii in range(ll-1):
+            result += abs(ord(s[ii])-ord(s[ii+1]))
+        return result
+
+if __name__ == "__main__":
+    app = Solution()
+    a = "hello"
+    print(app.leetcode(a))
