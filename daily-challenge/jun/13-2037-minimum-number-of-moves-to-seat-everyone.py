fe56fb1b8f5383d3c504e3dd8dd1e62da9b4a2a7
Xizhu Hou <houxizhu@gmail.com>
2024-06-13 13:19:23 +1000

    leetcode,daily-challenge/jun/13-2037-minimum-number-of-moves-to-seat-everyone.py

--- daily-challenge/jun/13-2037-minimum-number-of-moves-to-seat-everyone.py
+++ daily-challenge/jun/13-2037-minimum-number-of-moves-to-seat-everyone.py
@@ -1,36 +1,57 @@
 """
-75. Sort Colors
-Solved
-Medium
+2037. Minimum Number of Moves to Seat Everyone
+Easy
 Topics
 Companies
 Hint
-Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
 
-We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You may perform the following move any number of times:
 
-You must solve this problem without using the library's sort function.
+Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+
+Note that there may be multiple seats or students in the same position at the beginning.
 
 
 
 Example 1:
 
-Input: nums = [2,0,2,1,1,0]
-Output: [0,0,1,1,2,2]
+Input: seats = [3,1,5], students = [2,7,4]
+Output: 4
+Explanation: The students are moved as follows:
+- The first student is moved from from position 2 to position 1 using 1 move.
+- The second student is moved from from position 7 to position 5 using 2 moves.
+- The third student is moved from from position 4 to position 3 using 1 move.
+In total, 1 + 2 + 1 = 4 moves were used.
 Example 2:
 
-Input: nums = [2,0,1]
-Output: [0,1,2]
+Input: seats = [4,1,5,9], students = [1,3,2,6]
+Output: 7
+Explanation: The students are moved as follows:
+- The first student is not moved.
+- The second student is moved from from position 3 to position 4 using 1 move.
+- The third student is moved from from position 2 to position 5 using 3 moves.
+- The fourth student is moved from from position 6 to position 9 using 3 moves.
+In total, 0 + 1 + 3 + 3 = 7 moves were used.
+Example 3:
+
+Input: seats = [2,2,6,6], students = [1,3,2,6]
+Output: 4
+Explanation: Note that there are two seats at position 2 and two seats at position 6.
+The students are moved as follows:
+- The first student is moved from from position 1 to position 2 using 1 move.
+- The second student is moved from from position 3 to position 6 using 3 moves.
+- The third student is not moved.
+- The fourth student is not moved.
+In total, 1 + 3 + 0 + 0 = 4 moves were used.
 
 
 Constraints:
 
-n == nums.length
-1 <= n <= 300
-nums[i] is either 0, 1, or 2.
-
-
-Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+n == seats.length == students.length
+1 <= n <= 100
+1 <= seats[i], students[j] <= 100
 
 """
 
@@ -45,11 +66,15 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def leetcode(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        nums.sort()
+    def leetcode(self, eats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        n = len(seats)
+        result = 0
+        for ii in range(n):
+            result += abs(seats[ii]-students[ii])
+
+        return result
 
 if __name__ == "__main__":
     app = Solution()
