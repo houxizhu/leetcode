cccd740d5afa963bef5b5b1f9b8badb2cc5a1a4d
Xizhu Hou <houxizhu@gmail.com>
2024-06-14 11:36:45 +1000

    leetcode,easy,374-guess-number-higher-or-lower.py

---
+++ 374-guess-number-higher-or-lower.py
@@ -0,0 +1,81 @@
+"""
+374. Guess Number Higher or Lower
+Easy
+Topics
+Companies
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+
+
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        head = 1
+        tail = n
+        while head < tail:
+            middle = (head + tail) // 2
+            pick = guess(middle)
+            if pick < 0:
+                tail = middle - 1
+            elif pick > 0:
+                head = middle + 1
+            else:
+                return middle
+
+        return head
+
+if __name__ == "__main__":
+    app = Solution()
+    a = "hello"
+    a = " "
+    a = [4, 9, 5]
+    b = [9, 4, 9, 8, 4]
+    print(app.leetcode(a,b))
