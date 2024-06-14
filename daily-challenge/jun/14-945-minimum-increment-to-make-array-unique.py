b722a549f7a112a386a0e0d2b4d6d4bf5312cfb0
Xizhu Hou <houxizhu@gmail.com>
2024-06-14 13:37:44 +1000

    leetcode,14-945-minimum-increment-to-make-array-unique.py

---
+++ daily-challenge/jun/14-945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,69 @@
+"""
+945. Minimum Increment to Make Array Unique
+Medium
+Topics
+Companies
+You are given an integer array nums. In one move, you can pick an index i where 0 <= i < nums.length and increment nums[i] by 1.
+
+Return the minimum number of moves to make every value in nums unique.
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
+
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: 1
+Explanation: After 1 move, the array could be [1, 2, 3].
+Example 2:
+
+Input: nums = [3,2,1,2,1,7]
+Output: 6
+Explanation: After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+It can be shown with 5 or less moves that it is impossible for the array to have all unique values.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= nums[i] <= 105
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        result = 0
+        nums.sort()
+        next = nums[0] + 1
+
+        for ii in range(1,ll):
+            # print(ii,nums[ii],next)
+            if nums[ii] < next:
+                result += next-nums[ii]
+                next = next + 1
+            else:
+                next = nums[ii] + 1
+
+            # print(ii,result,next)
+
+        return result
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    a = [0,2,2]
+    #a = [3,2,1,2,1,7]
+    b = [5]
+    k = 5
+    print(app.leetcode(a))
