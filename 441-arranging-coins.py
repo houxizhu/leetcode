cb9661b82e6e6b71cde9a912be47a495488a5700
Xizhu Hou <houxizhu@gmail.com>
2024-06-26 00:07:38 +1000

    leetcode,easy,441-arranging-coins.py

---
+++ 441-arranging-coins.py
@@ -0,0 +1,57 @@
+"""
+441. Arranging Coins
+Solved
+Easy
+Topics
+Companies
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+
+
+Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+
+
+Constraints:
+
+1 <= n <= 231 - 1
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        result = 0
+        for ii in range(1, 1 << 31):
+            if ii > n:
+                break
+            n -= ii
+            result += 1
+
+        return result
+
+if __name__ == "__main__":
+    app = Solution()
+    a = "11"
+    b = "123"
+    print(app.leetcode(a,b))
