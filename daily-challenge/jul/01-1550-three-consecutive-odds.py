8107dec00cfcc33d467198f25c006f0e7e16d678
Xizhu Hou <houxizhu@gmail.com>
2024-07-01 13:48:24 +1000

    leetcode,daily-challenge/jul/01-1550-three-consecutive-odds.py

---
+++ daily-challenge/jul/01-1550-three-consecutive-odds.py
@@ -0,0 +1,64 @@
+"""
+1550. Three Consecutive Odds
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, arr: List[int]) -> bool:
+        result = 0
+        for each in arr:
+            if each&1 == 0:
+                result = 0
+            else:
+                result += 1
+            if result >= 3:
+                return True
+
+        return False
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    a = [0,2,2]
+    #a = [3,2,1,2,1,7]
+    b = [5]
+    k = 2
+    w = 0
+    profits = [1,2,3]
+    capital = [0,1,2]
+    print(app.leetcode(k,w,profits,capital))
