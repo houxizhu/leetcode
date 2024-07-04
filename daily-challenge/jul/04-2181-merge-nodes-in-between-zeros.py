181f40bd31e08ab9f8f7158a33402ec15b87e01f
Xizhu Hou <houxizhu@gmail.com>
2024-07-04 17:06:55 +1000

    leetcode,daily-challenge/jul/04-2181-merge-nodes-in-between-zeros.py

---
+++ daily-challenge/jul/04-2181-merge-nodes-in-between-zeros.py
@@ -0,0 +1,85 @@
+"""
+2181. Merge Nodes in Between Zeros
+Medium
+Topics
+Companies
+Hint
+You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+Return the head of the modified linked list.
+
+
+
+Example 1:
+
+
+Input: head = [0,3,1,0,4,5,2,0]
+Output: [4,11]
+Explanation:
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 3 + 1 = 4.
+- The sum of the nodes marked in red: 4 + 5 + 2 = 11.
+Example 2:
+
+
+Input: head = [0,1,0,3,0,2,2,0]
+Output: [1,3,4]
+Explanation:
+The above figure represents the given linked list. The modified list contains
+- The sum of the nodes marked in green: 1 = 1.
+- The sum of the nodes marked in red: 3 = 3.
+- The sum of the nodes marked in yellow: 2 + 2 = 4.
+
+
+Constraints:
+
+The number of nodes in the list is in the range [3, 2 * 105].
+0 <= Node.val <= 1000
+There are no two consecutive nodes with Node.val == 0.
+The beginning and end of the linked list have Node.val == 0.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        index0 = head
+        index1 = index0.next
+        while index1:
+            if index1.val == 0:
+                if index1.next == None:
+                    index0.next = None
+                    index1 = None
+                    break
+                else:
+                    index0.next = index1
+                    index0 = index1
+            else:
+                index0.val += index1.val
+            index1 = index1.next
+
+        return head
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    a = [0,2,2]
+    #a = [3,2,1,2,1,7]
+    b = [5]
+    k = 2
+    w = 0
+    profits = [1,2,3]
+    capital = [0,1,2]
+    print(app.leetcode(k,w,profits,capital))
