1d9c8d3dc64e9c123e001f20b14c0f386ee2519b
Xizhu Hou <houxizhu@gmail.com>
2024-07-06 00:05:49 +1000

    leetcode,easy,495-teemo-attacking.py

---
+++ 495-teemo-attacking.py
@@ -0,0 +1,63 @@
+"""
+495. Teemo Attacking
+Solved
+Easy
+Topics
+Companies
+Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
+
+You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
+
+Return the total number of seconds that Ashe is poisoned.
+
+
+
+Example 1:
+
+Input: timeSeries = [1,4], duration = 2
+Output: 4
+Explanation: Teemo's attacks on Ashe go as follows:
+- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+- At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+Example 2:
+
+Input: timeSeries = [1,2], duration = 2
+Output: 3
+Explanation: Teemo's attacks on Ashe go as follows:
+- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+- At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
+Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+
+
+Constraints:
+
+1 <= timeSeries.length <= 104
+0 <= timeSeries[i], duration <= 107
+timeSeries is sorted in non-decreasing order.
+"""
+
+import string
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, timeSeries: List[int], duration: int) -> int:
+        result = duration
+        ll = len(timeSeries)
+        for ii in range(1,ll):
+            result += min(duration, timeSeries[ii]-timeSeries[ii-1])
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = 5
+    b = [[1,2]]
+    print(app.leetcode(a,b))
