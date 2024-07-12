64368bc538fdc57eef4232a5242a5a395fd0d54c
Xizhu Hou <houxizhu@gmail.com>
2024-07-12 23:38:39 +1000

    leetcode,easy,506-relative-ranks.py

---
+++ 506-relative-ranks.py
@@ -0,0 +1,72 @@
+"""
+506. Relative Ranks
+Easy
+Topics
+Companies
+You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
+
+The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
+
+The 1st place athlete's rank is "Gold Medal".
+The 2nd place athlete's rank is "Silver Medal".
+The 3rd place athlete's rank is "Bronze Medal".
+For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
+Return an array answer of size n where answer[i] is the rank of the ith athlete.
+
+
+
+Example 1:
+
+Input: score = [5,4,3,2,1]
+Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
+Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+Example 2:
+
+Input: score = [10,3,8,9,4]
+Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+
+
+
+Constraints:
+
+n == score.length
+1 <= n <= 104
+0 <= score[i] <= 106
+All the values in score are unique.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, score: List[int]) -> List[str]:
+        ll = len(score)
+        lrank = [[score[ii], ii, 0] for ii in range(ll)]
+        lrank.sort(key=lambda x: x[0], reverse=True)
+        for ii in range(ll):
+            if ii == 0:
+                lrank[ii][2] = "Gold Medal"
+            elif ii == 1:
+                lrank[ii][2] = "Silver Medal"
+            elif ii == 2:
+                lrank[ii][2] = "Bronze Medal"
+            else:
+                lrank[ii][2] = str(ii + 1)
+        lrank.sort(key=lambda x: x[1])
+        result = [each[2] for each in lrank]
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
