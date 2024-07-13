9c0795d2b12ef69347bab423c9c00b7b851896fd
Xizhu Hou <houxizhu@gmail.com>
2024-07-14 00:04:10 +1000

    leetcode,easy,509-fibonacci-number.py

---
+++ 509-fibonacci-number.py
@@ -0,0 +1,58 @@
+"""
+509. Fibonacci Number
+Easy
+Topics
+Companies
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+
+Constraints:
+
+0 <= n <= 30
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        result = [0,1]
+        for ii in range(2,n+1):
+            result.append(result[ii-2]+result[ii-1])
+        return result[n]
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
