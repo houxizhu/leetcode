7329427d8d9c65a0df4b9118bfadc86a399ff0ad
Xizhu Hou <houxizhu@gmail.com>
2024-07-19 11:46:58 +1000

    leetcode,daily-challenge/jul/19-1380-lucky-numbers-in-a-matrix.py

---
+++ daily-challenge/jul/19-1380-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,76 @@
+"""
+1380. Lucky Numbers in a Matrix
+Easy
+Topics
+Companies
+Hint
+Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+
+A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+
+
+
+Example 1:
+
+Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+Output: [15]
+Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column.
+Example 2:
+
+Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+Output: [12]
+Explanation: 12 is the only lucky number since it is the minimum in its row and the maximum in its column.
+Example 3:
+
+Input: matrix = [[7,8],[1,2]]
+Output: [7]
+Explanation: 7 is the only lucky number since it is the minimum in its row and the maximum in its column.
+
+
+Constraints:
+
+m == mat.length
+n == mat[i].length
+1 <= n, m <= 50
+1 <= matrix[i][j] <= 105.
+All elements in the matrix are distinct.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, matrix: List[List[int]]) -> List[int]:
+        r = len(matrix)
+        c = len(matrix[0])
+        result = []
+
+        rowmin = 0
+        lrowmin = [min(each) for each in matrix]
+        # print(lrowmin)
+
+        for cc in range(c):
+            columnmax = matrix[0][cc]
+            columnmaxi = 0
+            for rr in range(r):
+                if matrix[rr][cc] > columnmax:
+                    columnmax = matrix[rr][cc]
+                    columnmaxi = rr
+            if columnmax == lrowmin[columnmaxi]:
+                result.append(columnmax)
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
