b4d46ced3a53f0eb5bae1a0b9aaca3f178e65d89
Xizhu Hou <houxizhu@gmail.com>
2024-07-28 00:14:54 +1000

    leetcode,easy,590-n-ary-tree-postorder-traversal.py

---
+++ 590-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,61 @@
+"""
+590. N-ary Tree Postorder Traversal
+Easy
+Topics
+Companies
+Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+
+
+Example 1:
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+Example 2:
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+0 <= Node.val <= 104
+The height of the n-ary tree is less than or equal to 1000.
+
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, children=None):
+        self.val = val
+        self.children = children
+
+class Solution:
+    def leetcode(self, root: 'Node') -> List[int]:
+        if root == None:
+            return []
+
+        result = []
+        for each in root.children:
+            result += self.postorder(each)
+        result += [root.val]
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
