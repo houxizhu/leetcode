d9fa4ba711bf2649dac478ef6d7760c2da506b7e
Xizhu Hou <houxizhu@gmail.com>
2024-08-01 11:22:47 +1000

    leetcode, daily-challenge/aug/01-2678-number-of-senior-citizens.py

---
+++ daily-challenge/aug/01-2678-number-of-senior-citizens.py
@@ -0,0 +1,64 @@
+"""
+2678. Number of Senior Citizens
+Easy
+Topics
+Companies
+Hint
+You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+The first ten characters consist of the phone number of passengers.
+The next character denotes the gender of the person.
+The following two characters are used to indicate the age of the person.
+The last two characters determine the seat allotted to that person.
+Return the number of passengers who are strictly more than 60 years old.
+
+
+
+Example 1:
+
+Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+Output: 2
+Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+Example 2:
+
+Input: details = ["1313579440F2036","2921522980M5644"]
+Output: 0
+Explanation: None of the passengers are older than 60.
+
+
+Constraints:
+
+1 <= details.length <= 100
+details[i].length == 15
+details[i] consists of digits from '0' to '9'.
+details[i][10] is either 'M' or 'F' or 'O'.
+The phone numbers and seat numbers of the passengers are distinct.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, details: List[str]) -> int:
+        result = 0
+        for each in details:
+            if each[11] == "6" and each[12] == "0":
+                continue
+            if each[11] in "6789":
+                result +=1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
