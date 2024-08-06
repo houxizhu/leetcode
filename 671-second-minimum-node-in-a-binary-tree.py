904c36ac896fdca9e06809d91056022c3b46b526
Xizhu Hou <houxizhu@gmail.com>
2024-08-07 00:03:28 +1000

    leetcode,easy,671-second-minimum-node-in-a-binary-tree.py

---
+++ 671-second-minimum-node-in-a-binary-tree.py
@@ -0,0 +1,69 @@
+"""
+671. Second Minimum Node In a Binary Tree
+Easy
+Topics
+Companies
+Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
+
+Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+
+If no such second minimum value exists, output -1 instead.
+
+
+
+
+
+Example 1:
+
+
+Input: root = [2,2,5,null,null,5,7]
+Output: 5
+Explanation: The smallest value is 2, the second smallest value is 5.
+Example 2:
+
+
+Input: root = [2,2,2]
+Output: -1
+Explanation: The smallest value is 2, but there isn't any second smallest value.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 25].
+1 <= Node.val <= 231 - 1
+root.val == min(root.left.val, root.right.val) for each internal node of the tree.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, root: Optional[TreeNode]) -> int:
+        q = [root]
+        values = []
+        while q:
+            p = q.pop()
+            if p.val > root.val:
+                values.append(p.val)
+            if p.left:
+                q.append(p.left)
+            if p.right:
+                q.append(p.right)
+        values.sort()
+        if len(values):
+            return values[0]
+        return -1
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
