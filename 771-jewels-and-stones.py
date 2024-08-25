ee077c8b55c458df08277a0bcef1cd09ef12da2a
Xizhu Hou <houxizhu@gmail.com>
2024-08-26 00:11:12 +1000

    leetcode,easy,771-jewels-and-stones.py

---
+++ 771-jewels-and-stones.py
@@ -0,0 +1,60 @@
+"""
+
+Code
+Testcase
+Test Result
+Test Result
+771. Jewels and Stones
+Solved
+Easy
+Topics
+Companies
+Hint
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, jewels: str, stones: str) -> int:
+        result = 0
+        for each in stones:
+            if each in jewels:
+                result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
