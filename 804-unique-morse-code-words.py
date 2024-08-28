2309879146bbb7cd90c2be4861212dde2d40f1db
Xizhu Hou <houxizhu@gmail.com>
2024-08-29 00:04:06 +1000

    leetcode,easy,804-unique-morse-code-words.py

---
+++ 804-unique-morse-code-words.py
@@ -0,0 +1,80 @@
+"""
+804. Unique Morse Code Words
+Solved
+Easy
+Topics
+Companies
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+Example 2:
+
+Input: words = ["a"]
+Output: 1
+
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 12
+words[i] consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, words: List[str]) -> int:
+        morse26 = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        dd = defaultdict(str)
+        for ii in range(26):
+            dd[letters[ii]] = morse26[ii]
+        lmorse = []
+        for word in words:
+            morse = ""
+            for char in word:
+                morse += dd[char]
+            lmorse.append(morse)
+
+        dd1 = defaultdict(int)
+        for each in lmorse:
+            dd1[each] += 1
+
+        return len(dd1.items())
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
