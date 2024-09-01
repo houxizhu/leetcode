394c8e2757ec254299e61c7142fc42478e2aa9dd
Xizhu Hou <houxizhu@gmail.com>
2024-09-01 11:51:07 +1000

    leetcode,daily-challenge/sep/01

---
+++ daily-challenge/sep/01-2022-convcert-1d-array-into-2d-array.py
@@ -0,0 +1,75 @@
+"""
+2022. Convert 1D Array Into 2D Array
+Easy
+Topics
+Companies
+Hint
+You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with  m rows and n columns using all the elements from original.
+
+The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
+
+Return an m x n 2D array constructed according to the above procedure, or an empty 2D array if it is impossible.
+
+
+
+Example 1:
+
+
+Input: original = [1,2,3,4], m = 2, n = 2
+Output: [[1,2],[3,4]]
+Explanation: The constructed 2D array should contain 2 rows and 2 columns.
+The first group of n=2 elements in original, [1,2], becomes the first row in the constructed 2D array.
+The second group of n=2 elements in original, [3,4], becomes the second row in the constructed 2D array.
+Example 2:
+
+Input: original = [1,2,3], m = 1, n = 3
+Output: [[1,2,3]]
+Explanation: The constructed 2D array should contain 1 row and 3 columns.
+Put all three elements in original into the first row of the constructed 2D array.
+Example 3:
+
+Input: original = [1,2], m = 1, n = 1
+Output: []
+Explanation: There are 2 elements in original.
+It is impossible to fit 2 elements in a 1x1 2D array, so return an empty 2D array.
+
+
+Constraints:
+
+1 <= original.length <= 5 * 104
+1 <= original[i] <= 105
+1 <= m, n <= 4 * 104
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        ll = len(original)
+        if ll != m*n:
+            return []
+        if m == 1:
+            return [original]
+
+        result = [[0 for _ in range(n)] for _ in range(m)]
+        for ii in range(ll):
+            mm = ii//n
+            nn = ii%n
+            print(ii,mm,nn)
+            result[mm][nn] = original[ii]
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
