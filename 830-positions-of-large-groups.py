5666ce5bfb08aac08f8dc3c01eb8a769500dd9b2
Xizhu Hou <houxizhu@gmail.com>
2024-09-03 00:21:33 +1000

    leetcode,easy,830-positions-of-large-groups.py

---
+++ 830-positions-of-large-groups.py
@@ -0,0 +1,75 @@
+"""
+830. Positions of Large Groups
+Easy
+Topics
+Companies
+In a string s of lowercase letters, these letters form consecutive groups of the same character.
+
+For example, a string like s = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z", and "yy".
+
+A group is identified by an interval [start, end], where start and end denote the start and end indices (inclusive) of the group. In the above example, "xxxx" has the interval [3,6].
+
+A group is considered large if it has 3 or more characters.
+
+Return the intervals of every large group sorted in increasing order by start index.
+
+
+
+Example 1:
+
+Input: s = "abbxxxxzzy"
+Output: [[3,6]]
+Explanation: "xxxx" is the only large group with start index 3 and end index 6.
+Example 2:
+
+Input: s = "abc"
+Output: []
+Explanation: We have groups "a", "b", and "c", none of which are large groups.
+Example 3:
+
+Input: s = "abcdddeeeeaabbbcd"
+Output: [[3,5],[6,9],[12,14]]
+Explanation: The large groups are "ddd", "eeee", and "bbb".
+
+
+Constraints:
+
+1 <= s.length <= 1000
+s contains lowercase English letters only.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> List[List[int]]:
+        result = []
+        ll = len(s)
+        count = 1
+        start = 0
+        for ii in range(1,ll):
+            if s[ii] == s[ii-1]:
+                count += 1
+            else:
+                if count >= 3:
+                    result.append([start,ii-1])
+                start = ii
+                count = 1
+        if count >= 3:
+            result.append([start,ll-1])
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
