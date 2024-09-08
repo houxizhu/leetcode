c67a29051efd748360d36f768b27b85a56bbac46
Xizhu Hou <houxizhu@gmail.com>
2024-09-08 11:39:33 +1000

    leetcode,daily-challenge/sep/08-725-split-linked-list-parts.py

---
+++ daily-challenge/sep/08-725-split-linked-list-parts.py
@@ -0,0 +1,84 @@
+"""
+725. Split Linked List in Parts
+Medium
+Topics
+Companies
+Hint
+Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts.
+
+The length of each part should be as equal as possible: no two parts should have a size differing by more than one. This may lead to some parts being null.
+
+The parts should be in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
+
+Return an array of the k parts.
+
+
+
+Example 1:
+
+
+Input: head = [1,2,3], k = 5
+Output: [[1],[2],[3],[],[]]
+Explanation:
+The first element output[0] has output[0].val = 1, output[0].next = null.
+The last element output[4] is null, but its string representation as a ListNode is [].
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
+Output: [[1,2,3,4],[5,6,7],[8,9,10]]
+Explanation:
+The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 1000].
+0 <= Node.val <= 1000
+1 <= k <= 50
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+        result = []
+        count = 0
+        index = head
+        while index:
+            count += 1
+            index = index.next
+        kcount = count//k
+        reminder = count%k
+        while head:
+            p = None
+            result.append(head)
+            for ii in range(kcount):
+                if head:
+                    p = head
+                    head = head.next
+            if reminder:
+                if head:
+                    p = head
+                    head = head.next
+                    reminder -= 1
+            p.next = None
+        while len(result) < k:
+            result.append(None)
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
