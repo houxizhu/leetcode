5984ad781ba535f7b1b09a759367bd509e401946
Xizhu Hou <houxizhu@gmail.com>
2024-09-13 20:07:20 +1000

    leetcode,daily-challenge/sep/13-1310-xor-queries-of-a-subarray.py

---
+++ daily-challenge/sep/13-1310-xor-queries-of-a-subarray.py
@@ -0,0 +1,83 @@
+"""
+1310. XOR Queries of a Subarray
+Medium
+Topics
+Companies
+Hint
+You are given an array arr of positive integers. You are also given the array queries where queries[i] = [lefti, righti].
+
+For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
+
+Return an array answer where answer[i] is the answer to the ith query.
+
+
+
+Example 1:
+
+Input: arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+Output: [2,7,14,8]
+Explanation:
+The binary representation of the elements in the array are:
+1 = 0001
+3 = 0011
+4 = 0100
+8 = 1000
+The XOR values for queries are:
+[0,1] = 1 xor 3 = 2
+[1,2] = 3 xor 4 = 7
+[0,3] = 1 xor 3 xor 4 xor 8 = 14
+[3,3] = 8
+Example 2:
+
+Input: arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+Output: [8,0,4,4]
+
+
+Constraints:
+
+1 <= arr.length, queries.length <= 3 * 104
+1 <= arr[i] <= 109
+queries[i].length == 2
+0 <= lefti <= righti < arr.length
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        lla = len(arr)
+        ll = len(queries)
+        matrix = [0 for _ in range(lla)]
+        matrix[0] = arr[0]
+        for ii in range(1,lla):
+            matrix[ii] = matrix[ii-1] ^ arr[ii]
+
+        ### memory limit exceeded
+        # for ii in range(lla):
+        #     for jj in range(ii, lla):
+        #         if jj == ii:
+        #             matrix[ii][jj] = arr[jj]
+        #         else:
+        #             matrix[ii][jj] = matrix[ii][jj-1] ^ arr[jj]
+
+        result = [0] * ll
+        for ii in range(ll):
+            left,right = queries[ii]
+            result[ii] = matrix[left] ^ matrix[right] ^ arr[left]
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
