fc0ea54647228264b74f9470552857466d534b18
Xizhu Hou <houxizhu@gmail.com>
2024-09-15 00:17:26 +1000

    leetcode,easy,896-monotonic-array.py

---
+++ 896-monotonic-array.py
@@ -0,0 +1,63 @@
+"""
+896. Monotonic Array
+Easy
+Topics
+Companies
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+
+
+Example 1:
+
+Input: nums = [1,2,2,3]
+Output: true
+Example 2:
+
+Input: nums = [6,5,4,4]
+Output: true
+Example 3:
+
+Input: nums = [1,3,2]
+Output: false
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-105 <= nums[i] <= 105
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> bool:
+        ll = len(nums)
+        gt = lt = eq = 0
+        for ii in range(1,ll):
+            if nums[ii] > nums[ii-1]:
+                gt = 1
+            elif nums[ii] < nums[ii-1]:
+                lt = 1
+        if gt+lt == 2:
+            return False
+
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
