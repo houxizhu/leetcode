47df5083ac83973c71d10d6d3bdf80e6574457c8
Xizhu Hou <houxizhu@gmail.com>
2024-09-28 20:12:08 +1000

    leetcode,easy,944-delete-columns-to-make-sorted.py

---
+++ 944-delete-columns-to-make-sorted.py
@@ -0,0 +1,87 @@
+"""
+944. Delete Columns to Make Sorted
+Easy
+Topics
+Companies
+You are given an array of n strings strs, all of the same length.
+
+The strings can be arranged such that there is one on each line, making a grid.
+
+For example, strs = ["abc", "bce", "cae"] can be arranged as follows:
+abc
+bce
+cae
+You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted, while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+Return the number of columns that you will delete.
+
+
+
+Example 1:
+
+Input: strs = ["cba","daf","ghi"]
+Output: 1
+Explanation: The grid looks as follows:
+  cba
+  daf
+  ghi
+Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
+Example 2:
+
+Input: strs = ["a","b"]
+Output: 0
+Explanation: The grid looks as follows:
+  a
+  b
+Column 0 is the only column and is sorted, so you will not delete any columns.
+Example 3:
+
+Input: strs = ["zyx","wvu","tsr"]
+Output: 3
+Explanation: The grid looks as follows:
+  zyx
+  wvu
+  tsr
+All 3 columns are not sorted, so you will delete all 3.
+
+
+Constraints:
+
+n == strs.length
+1 <= n <= 100
+1 <= strs[i].length <= 1000
+strs[i] consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, strs: List[str]) -> int:
+        rr = len(strs)
+        cc = len(strs[0])
+        result = 0
+        for c in range(cc):
+            flag = 0
+            for r in range(1,rr):
+                if ord(strs[r][c]) < ord(strs[r-1][c]):
+                    flag = 1
+                    break
+            if flag == 1:
+                result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
