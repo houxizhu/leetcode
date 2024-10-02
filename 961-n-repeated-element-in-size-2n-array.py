c97697a40e9105daae3dcdb915f9f1dbbab5e364
Xizhu Hou <xhou@iriusrisk.com>
2024-10-02 23:01:46 +0800

    leetcode,easy,961-n-repeated-element-in-size-2n-array.py

---
+++ 961-n-repeated-element-in-size-2n-array.py
@@ -0,0 +1,62 @@
+"""
+961. N-Repeated Element in Size 2N Array
+Easy
+Topics
+Companies
+You are given an integer array nums with the following properties:
+
+nums.length == 2 * n.
+nums contains n + 1 unique elements.
+Exactly one element of nums is repeated n times.
+Return the element that is repeated n times.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,1,2,5,3,2]
+Output: 2
+Example 3:
+
+Input: nums = [5,1,5,2,5,3,5,4]
+Output: 5
+
+
+Constraints:
+
+2 <= n <= 5000
+nums.length == 2 * n
+0 <= nums[i] <= 104
+nums contains n + 1 unique elements and one of them is repeated exactly n times.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        nums.sort()
+        for ii in range(1,ll):
+            if nums[ii] == nums[ii-1]:
+                return nums[ii]
+
+        return nums[0]
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
