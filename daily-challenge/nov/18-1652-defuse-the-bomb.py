33997290a5f67ff67f3b39227a229bb456a95cf4
Xizhu Hou <houxizhu@gmail.com>
2024-11-18 14:24:06 +1100

    leetcode,daily-challenge/nov/18-1652-defuse-the-bomb.py

---
+++ daily-challenge/nov/18-1652-defuse-the-bomb.py
@@ -0,0 +1,79 @@
+"""
+1652. Defuse the Bomb
+Easy
+Topics
+Companies
+Hint
+You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
+
+To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+
+If k > 0, replace the ith number with the sum of the next k numbers.
+If k < 0, replace the ith number with the sum of the previous k numbers.
+If k == 0, replace the ith number with 0.
+As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+
+Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+
+ 
+
+Example 1:
+
+Input: code = [5,7,1,4], k = 3
+Output: [12,10,16,13]
+Explanation: Each number is replaced by the sum of the next 3 numbers. The decrypted code is [7+1+4, 1+4+5, 4+5+7, 5+7+1]. Notice that the numbers wrap around.
+Example 2:
+
+Input: code = [1,2,3,4], k = 0
+Output: [0,0,0,0]
+Explanation: When k is zero, the numbers are replaced by 0. 
+Example 3:
+
+Input: code = [2,4,9,3], k = -2
+Output: [12,5,6,13]
+Explanation: The decrypted code is [3+9, 2+3, 4+2, 9+4]. Notice that the numbers wrap around again. If k is negative, the sum is of the previous numbers.
+ 
+
+Constraints:
+
+n == code.length
+1 <= n <= 100
+1 <= code[i] <= 100
+-(n - 1) <= k <= n - 1
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, code: List[int], k: int) -> List[int]:
+        ll = len(code)
+        result = [0]*ll
+        if k == 0:
+            return result
+        
+        ksum = sum(code[0:abs(k)])
+        for ii in range(ll):
+            ksum -= code[ii]
+            ksum += code[(abs(k)+ii)%ll]
+            if k > 0:
+                result[ii] = ksum
+            elif k < 0:
+                result[(abs(k)+ii+1)%ll] = ksum
+
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
