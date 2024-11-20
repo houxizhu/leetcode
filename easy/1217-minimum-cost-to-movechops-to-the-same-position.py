1b14472c15834c8e9268dd415c16c7ce2e971a87
Xizhu Hou <houxizhu@gmail.com>
2024-11-20 11:34:44 +1100

    leetcode,easy/1217-minimum-cost-to-movechops-to-the-same-position.py

---
+++ easy/1217-minimum-cost-to-movechops-to-the-same-position.py
@@ -0,0 +1,72 @@
+"""
+1217. Minimum Cost to Move Chips to The Same Position
+Solved
+Easy
+Topics
+Companies
+Hint
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+
+ 
+
+Example 1:
+
+
+Input: position = [1,2,3]
+Output: 1
+Explanation: First step: Move the chip at position 3 to position 1 with cost = 0.
+Second step: Move the chip at position 2 to position 1 with cost = 1.
+Total cost is 1.
+Example 2:
+
+
+Input: position = [2,2,2,3,3]
+Output: 2
+Explanation: We can move the two chips at position  3 to position 2. Each move has cost = 1. The total cost = 2.
+Example 3:
+
+Input: position = [1,1000000000]
+Output: 1
+ 
+
+Constraints:
+
+1 <= position.length <= 100
+1 <= position[i] <= 10^9
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, position: List[int]) -> int:
+        ll = len(position)
+        count0 = 0
+        count1 = 0
+        for ii in range(ll):
+            if position[ii]%2 == 0:
+                count0 += 1
+            else:
+                count1 += 1
+
+        return min(count0, count1)
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
