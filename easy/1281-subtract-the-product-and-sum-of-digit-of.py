85e284e968285e116a56f11b475e8b1c6a15977a
Xizhu Hou <houxizhu@gmail.com>
2024-11-26 01:44:14 +1100

    leetcode,easy/1281-subtract-the-product-and-sum-of-digit-of.py

---
+++ easy/1281-subtract-the-product-and-sum-of-digit-of.py
@@ -0,0 +1,61 @@
+"""
+1281. Subtract the Product and Sum of Digits of an Integer
+Easy
+Topics
+Companies
+Hint
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+ 
+
+Constraints:
+
+1 <= n <= 10^5
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, n: int) -> int:
+        product = 1
+        sum = 0
+        while n > 0:
+            digit = n%10
+            n //= 10
+            product *= digit
+            sum += digit
+
+        return product-sum
+
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
