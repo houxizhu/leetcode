c538a05c447e1c22a22151b4c87a41686faa1d20
Xizhu Hou <houxizhu@gmail.com>
2024-12-13 14:44:47 +1100

    leetcode,daily-challenge/dec/13-2593-find-score-of-an-array-after-marking-all-elements.py

---
+++ daily-challenge/dec/13-2593-find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,79 @@
+"""
+2593. Find Score of an Array After Marking All Elements
+Attempted
+Medium
+Topics
+Companies
+Hint
+You are given an array nums consisting of positive integers.
+
+Starting with score = 0, apply the following algorithm:
+
+Choose the smallest integer of the array that is not marked. If there is a tie, choose the one with the smallest index.
+Add the value of the chosen integer to score.
+Mark the chosen element and its two adjacent elements if they exist.
+Repeat until all the array elements are marked.
+Return the score you get after applying the above algorithm.
+
+ 
+
+Example 1:
+
+Input: nums = [2,1,3,4,5,2]
+Output: 7
+Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,1,3,4,5,2].
+- 2 is the smallest unmarked element, so we mark it and its left adjacent element: [2,1,3,4,5,2].
+- 4 is the only remaining unmarked element, so we mark it: [2,1,3,4,5,2].
+Our score is 1 + 2 + 4 = 7.
+Example 2:
+
+Input: nums = [2,3,5,1,3,2]
+Output: 5
+Explanation: We mark the elements as follows:
+- 1 is the smallest unmarked element, so we mark it and its two adjacent elements: [2,3,5,1,3,2].
+- 2 is the smallest unmarked element, since there are two of them, we choose the left-most one, so we mark the one at index 0 and its right adjacent element: [2,3,5,1,3,2].
+- 2 is the only remaining unmarked element, so we mark it: [2,3,5,1,3,2].
+Our score is 1 + 2 + 2 = 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        ll = len(nums)
+        result = 0
+        grid = [[nums[ii],ii,ii-1,ii+1] for ii in range(ll)]
+        grid.sort()
+        marked = [0 for _ in range(ll)]
+        for ii in range(ll):
+            if marked[grid[ii][1]] == 0:
+                result += grid[ii][0]
+                marked[grid[ii][1]] = 1
+                if grid[ii][1] > 0:
+                    marked[grid[ii][2]] = 1
+                if grid[ii][1] < ll-1:
+                    marked[grid[ii][3]] = 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
