d77ee383a966eb7a4001843d970e1fbbd6bac714
Xizhu Hou <houxizhu@gmail.com>
2024-12-17 21:54:41 +1100

    leetcode,daily-challenge/dec/17-2182-construct-string-with-repeat-limit.py

---
+++ daily-challenge/dec/17-2182-construct-string-with-repeat-limit.py
@@ -0,0 +1,91 @@
+"""
+2182. Construct String With Repeat Limit
+Medium
+Topics
+Companies
+Hint
+You are given a string s and an integer repeatLimit. Construct a new string repeatLimitedString using the characters of s such that no letter appears more than repeatLimit times in a row. You do not have to use all characters from s.
+
+Return the lexicographically largest repeatLimitedString possible.
+
+A string a is lexicographically larger than a string b if in the first position where a and b differ, string a has a letter that appears later in the alphabet than the corresponding letter in b. If the first min(a.length, b.length) characters do not differ, then the longer string is the lexicographically larger one.
+
+ 
+
+Example 1:
+
+Input: s = "cczazcc", repeatLimit = 3
+Output: "zzcccac"
+Explanation: We use all of the characters from s to construct the repeatLimitedString "zzcccac".
+The letter 'a' appears at most 1 time in a row.
+The letter 'c' appears at most 3 times in a row.
+The letter 'z' appears at most 2 times in a row.
+Hence, no letter appears more than repeatLimit times in a row and the string is a valid repeatLimitedString.
+The string is the lexicographically largest repeatLimitedString possible so we return "zzcccac".
+Note that the string "zzcccca" is lexicographically larger but the letter 'c' appears more than 3 times in a row, so it is not a valid repeatLimitedString.
+Example 2:
+
+Input: s = "aababab", repeatLimit = 2
+Output: "bbabaa"
+Explanation: We use only some of the characters from s to construct the repeatLimitedString "bbabaa". 
+The letter 'a' appears at most 2 times in a row.
+The letter 'b' appears at most 2 times in a row.
+Hence, no letter appears more than repeatLimit times in a row and the string is a valid repeatLimitedString.
+The string is the lexicographically largest repeatLimitedString possible so we return "bbabaa".
+Note that the string "bbabaaa" is lexicographically larger but the letter 'a' appears more than 2 times in a row, so it is not a valid repeatLimitedString.
+ 
+
+Constraints:
+
+1 <= repeatLimit <= s.length <= 105
+s consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str, repeatLimit: int) -> str:
+        ll = len(s)
+        q = [0]*26
+        za = "zyxwvutsrqponmlkjihgfedcba"
+        for each in s:
+            q[25-(ord(each)-ord("a"))] += 1
+        
+        first = 0
+        second = 0
+        result = ""
+        while first < 26:
+            if q[first] == 0:
+                first += 1
+            else:
+                repeated = min(q[first],repeatLimit)
+                result += za[first]*repeated
+                q[first] -= repeated
+                if q[first] == 0:
+                    first += 1
+                    continue
+
+                second = first+1
+                while second < 26 and q[second] == 0:
+                    second += 1
+                if second == 26:
+                    return result
+                result += za[second]
+                q[second] -= 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
