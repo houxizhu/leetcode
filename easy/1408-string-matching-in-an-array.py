494b42018e029cfbbc7680a0a5e3ff15c1521fe0
Xizhu Hou <houxizhu@gmail.com>
2024-12-26 00:04:08 +1100

    leetcode,easy/1408-string-matching-in-an-array.py

---
+++ easy/1408-string-matching-in-an-array.py
@@ -0,0 +1,67 @@
+"""
+1408. String Matching in an Array
+Easy
+Topics
+Companies
+Hint
+Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
+
+A substring is a contiguous sequence of characters within a string
+
+ 
+
+Example 1:
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+Explanation: No string of words is substring of another string.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 30
+words[i] contains only lowercase English letters.
+All the strings of words are unique.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, words: List[str]) -> List[str]:
+        ll = len(words)
+        result = []
+        words.sort(key=lambda x:len(x))
+        for ii in range(ll-1):
+            for jj in range(ii+1,ll):
+                if words[ii] in words[jj]:
+                    result.append(words[ii])
+                    break
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
