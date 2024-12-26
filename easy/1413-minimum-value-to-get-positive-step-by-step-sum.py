d0b0a809d2d254b2d905dd8c3a1d8dfd0d5833db
Xizhu Hou <houxizhu@gmail.com>
2024-12-27 00:34:34 +1100

    leetcode,easy/1413-minimum-value-to-get-positive-step-by-step-sum.py

---
+++ easy/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -0,0 +1,75 @@
+"""
+
+Code
+Testcase
+Test Result
+Test Result
+1413. Minimum Value to Get Positive Step by Step Sum
+Easy
+Topics
+Companies
+Hint
+Given an array of integers nums, you start with an initial positive value startValue.
+
+In each iteration, you calculate the step by step sum of startValue plus elements in nums (from left to right).
+
+Return the minimum positive value of startValue such that the step by step sum is never less than 1.
+
+ 
+
+Example 1:
+
+Input: nums = [-3,2,-3,4,2]
+Output: 5
+Explanation: If you choose startValue = 4, in the third iteration your step by step sum is less than 1.
+step by step sum
+startValue = 4 | startValue = 5 | nums
+  (4 -3 ) = 1  | (5 -3 ) = 2    |  -3
+  (1 +2 ) = 3  | (2 +2 ) = 4    |   2
+  (3 -3 ) = 0  | (4 -3 ) = 1    |  -3
+  (0 +4 ) = 4  | (1 +4 ) = 5    |   4
+  (4 +2 ) = 6  | (5 +2 ) = 7    |   2
+Example 2:
+
+Input: nums = [1,2]
+Output: 1
+Explanation: Minimum start value should be positive. 
+Example 3:
+
+Input: nums = [1,-2,-3]
+Output: 5
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        max_negtive = 0
+        total = 0
+        for each in nums:
+            total += each
+            max_negtive = min(max_negtive, total)
+
+        return 0-(max_negtive-1)
+
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
