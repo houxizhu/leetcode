21bb60a28ec913fbca4cf9f28dd48a9b8f0dd8e0
Xizhu Hou <houxizhu@gmail.com>
2025-01-02 00:03:28 +1100

    leetcode,easy/1446-consecutive-characters.py

---
+++ easy/1446-consecutive-characters.py
@@ -0,0 +1,61 @@
+"""
+1446. Consecutive Characters
+Solved
+Easy
+Topics
+Companies
+Hint
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Given a string s, return the power of s.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 2
+Explanation: The substring "ee" is of length 2 with the character 'e' only.
+Example 2:
+
+Input: s = "abbcccddddeeeeedcba"
+Output: 5
+Explanation: The substring "eeeee" is of length 5 with the character 'e' only.
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of only lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        result = 1
+        ll = len(s)
+        count = 1
+        for ii in range(1,ll):
+            if s[ii] == s[ii-1]:
+                count += 1
+                result = max(result,count)
+            else:
+                count = 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
