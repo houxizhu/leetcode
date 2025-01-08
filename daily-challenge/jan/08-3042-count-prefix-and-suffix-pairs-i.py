58239cead8aa9d52dfaa069fb6e201c863a5e41d
Xizhu Hou <houxizhu@gmail.com>
2025-01-08 23:08:21 +1100

    leetcode, daily-challenge/jan/08-3042-count-prefix-and-suffix-pairs-i.py

---
+++ daily-challenge/jan/08-3042-count-prefix-and-suffix-pairs-i.py
@@ -0,0 +1,96 @@
+"""
+3042. Count Prefix and Suffix Pairs I
+Easy
+Topics
+Companies
+Hint
+You are given a 0-indexed string array words.
+
+Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+
+isPrefixAndSuffix(str1, str2) returns true if str1 is both a 
+prefix
+ and a 
+suffix
+ of str2, and false otherwise.
+For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of "ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+
+Return an integer denoting the number of index pairs (i, j) such that i < j, and isPrefixAndSuffix(words[i], words[j]) is true.
+
+ 
+
+Example 1:
+
+Input: words = ["a","aba","ababa","aa"]
+Output: 4
+Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("a", "aba") is true.
+i = 0 and j = 2 because isPrefixAndSuffix("a", "ababa") is true.
+i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true.
+i = 1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true.
+Therefore, the answer is 4.
+Example 2:
+
+Input: words = ["pa","papa","ma","mama"]
+Output: 2
+Explanation: In this example, the counted index pairs are:
+i = 0 and j = 1 because isPrefixAndSuffix("pa", "papa") is true.
+i = 2 and j = 3 because isPrefixAndSuffix("ma", "mama") is true.
+Therefore, the answer is 2.  
+Example 3:
+
+Input: words = ["abab","ab"]
+Output: 0
+Explanation: In this example, the only valid index pair is i = 0 and j = 1, and isPrefixAndSuffix("abab", "ab") is false.
+Therefore, the answer is 0.
+ 
+
+Constraints:
+
+1 <= words.length <= 50
+1 <= words[i].length <= 10
+words[i] consists only of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, words: List[str]) -> int:
+        def isPrefixAndSuffix(str1, str2) -> bool:
+            ll1 = len(str1)
+            ll2 = len(str2)
+            if ll1 > ll2:
+                return False
+            if ll1 == ll2:
+                if str1 == str2:
+                    return True
+                return False
+
+            if str1 == str2[:ll1]:
+                if str1 == str2[ll2-ll1:]:
+                    return True
+            return False
+
+        result = 0
+        ll = len(words)
+        for ii in range(ll-1):
+            for jj in range(ii+1,ll):
+                if isPrefixAndSuffix(words[ii], words[jj]):
+                    result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
