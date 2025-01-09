c442d83fed4ae515519a1246dbbaa9ec4ed5979d
Xizhu Hou <houxizhu@gmail.com>
2025-01-10 02:07:11 +1100

    leetcode,easy/1480-running-sum-of-1d-array.py

---
+++ easy/1480-running-sum-of-1d-array.py
@@ -0,0 +1,59 @@
+"""
+1480. Running Sum of 1d Array
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> List[int]:
+        ll = len(nums)
+        for ii in range(1,ll):
+            nums[ii] += nums[ii-1]
+
+        return nums
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
