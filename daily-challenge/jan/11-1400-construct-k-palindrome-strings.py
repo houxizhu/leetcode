c735b9a75e68eb7896502763ed04a17f0e306c67
Xizhu Hou <houxizhu@gmail.com>
2025-01-11 18:13:06 +1100

    leetcode, daily-challenge/jan/11-1400-construct-k-palindrome-strings.py

---
+++ daily-challenge/jan/11-1400-construct-k-palindrome-strings.py
@@ -0,0 +1,69 @@
+"""
+1400. Construct K Palindrome Strings
+Medium
+Topics
+Companies
+Hint
+Given a string s and an integer k, return true if you can use all the characters in s to construct k palindrome strings or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "annabelle", k = 2
+Output: true
+Explanation: You can construct two palindromes using all characters in s.
+Some possible constructions "anna" + "elble", "anbna" + "elle", "anellena" + "b"
+Example 2:
+
+Input: s = "leetcode", k = 3
+Output: false
+Explanation: It is impossible to construct 3 palindromes using all the characters of s.
+Example 3:
+
+Input: s = "true", k = 4
+Output: true
+Explanation: The only possible solution is to put each character in a separate string.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= 105
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str, k: int) -> bool:
+        ll = len(s)
+        if ll < k:
+            return False
+        dd = defaultdict(int)
+        for each in s:
+            dd[each] += 1
+        
+        odd_count = 0
+        for each in dd:
+            if dd[each]%2 == 1:
+                odd_count += 1
+        if odd_count > k:
+            return False
+
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
