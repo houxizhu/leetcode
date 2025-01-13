add0405a13296283744312a63593165aacb07348
Xizhu Hou <houxizhu@gmail.com>
2025-01-14 00:18:18 +1100

    leetcode,easy/1502-can-make-arithmetic-progression-from-sequence.py

---
+++ easy/1502-can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,31 @@
+"""
+
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, arr: List[int]) -> bool:
+        ll = len(arr)
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for ii in range(2,ll):
+            if arr[ii] - arr[ii-1] != diff:
+                return False
+
+        return True
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
