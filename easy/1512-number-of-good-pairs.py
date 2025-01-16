9fb98f5ffc6aca526ce0fd95ea65857744823fd6
Xizhu Hou <houxizhu@gmail.com>
2025-01-16 15:08:13 +1100

    leetcode,easy/1512-number-of-good-pairs.py

---
+++ easy/1512-number-of-good-pairs.py
@@ -0,0 +1,62 @@
+"""
+1512. Number of Good Pairs
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        result = 0
+        ll = len(nums)
+        for ii in range(ll-1):
+            for jj in range(ii+1,ll):
+                if nums[ii] == nums[jj]:
+                    result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
