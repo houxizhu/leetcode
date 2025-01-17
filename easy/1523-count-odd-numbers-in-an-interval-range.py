c0562429bab1cf22e2a9ac2da60a09dbcb2ce4e9
Xizhu Hou <houxizhu@gmail.com>
2025-01-18 08:54:37 +1100

    leetcode,easy/1523-count-odd-numbers-in-an-interval-range.py

---
+++ easy/1523-count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,53 @@
+"""
+1523. Count Odd Numbers in an Interval Range
+Solved
+Easy
+Topics
+Companies
+Hint
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+ 
+
+Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+Example 2:
+
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+ 
+
+Constraints:
+
+0 <= low <= high <= 10^9
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, low: int, high: int) -> int:
+        result = (high+1-low)//2
+
+        if (high+1-low)%2 == 1 and low%2 == 1:
+            result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
