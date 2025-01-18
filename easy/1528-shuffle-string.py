5a5697824c2eedc7d7a15e44784d307ef1374bbb
Xizhu Hou <houxizhu@gmail.com>
2025-01-19 10:06:54 +1100

    leetcode,easy/1528-shuffle-string.py

---
+++ easy/1528-shuffle-string.py
@@ -0,0 +1,60 @@
+"""
+1528. Shuffle String
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+ 
+
+Example 1:
+
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+ 
+
+Constraints:
+
+s.length == indices.length == n
+1 <= n <= 100
+s consists of only lowercase English letters.
+0 <= indices[i] < n
+All values of indices are unique.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str, indices: List[int]) -> str:
+        ll = len(s)
+        result = ["a"]*ll
+        for ii in range(ll):
+            result[indices[ii]] = s[ii]
+
+        return "".join(result)
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
