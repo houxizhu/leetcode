f864e0688eacacea9df5124f6a4c4b9649498a0b
Xizhu Hou <houxizhu@gmail.com>
2025-01-20 11:53:02 +1100

    leetcode,easy/1534-count-good-triplets.py

---
+++ easy/1534-count-good-triplets.py
@@ -0,0 +1,73 @@
+"""
+1534. Count Good Triplets
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array of integers arr, and three integers a, b and c. You need to find the number of good triplets.
+
+A triplet (arr[i], arr[j], arr[k]) is good if the following conditions are true:
+
+0 <= i < j < k < arr.length
+|arr[i] - arr[j]| <= a
+|arr[j] - arr[k]| <= b
+|arr[i] - arr[k]| <= c
+Where |x| denotes the absolute value of x.
+
+Return the number of good triplets.
+
+ 
+
+Example 1:
+
+Input: arr = [3,0,1,1,9,7], a = 7, b = 2, c = 3
+Output: 4
+Explanation: There are 4 good triplets: [(3,0,1), (3,0,1), (3,1,1), (0,1,1)].
+Example 2:
+
+Input: arr = [1,1,2,2,3], a = 0, b = 0, c = 1
+Output: 0
+Explanation: No triplet satisfies all conditions.
+ 
+
+Constraints:
+
+3 <= arr.length <= 100
+0 <= arr[i] <= 1000
+0 <= a, b, c <= 1000
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, arr: List[int], a: int, b: int, c: int) -> int:
+        ll = len(arr)
+        result = 0
+        for ii in range(ll-2):
+            for jj in range(ii+1, ll-1):
+                if abs(arr[jj]-arr[ii]) > a:
+                    continue
+                for kk in range(jj+1, ll):
+                    if abs(arr[kk]-arr[jj]) > b:
+                        continue
+                    if abs(arr[kk]-arr[ii]) > c:
+                        continue
+                    result += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
