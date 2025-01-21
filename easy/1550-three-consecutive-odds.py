7fa712505b4789cb964258d86852cc07db329327
Xizhu Hou <houxizhu@gmail.com>
2025-01-22 10:03:14 +1100

    easy/1550-three-consecutive-odds.py

--- easy/1550-three-consecutive-odds.py
+++ easy/1550-three-consecutive-odds.py
@@ -1,5 +1,29 @@
 """
+1550. Three Consecutive Odds
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+ 
 
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
 """
 
 import string
@@ -14,14 +38,15 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def leetcode(self, head: Optional[ListNode]) -> List[int]:
-        ll = len(nums)
-        result = 0
+    def leetcode(self, arr: List[int]) -> bool:
+        ll = len(arr)
+        for ii in range(ll-2):
+            if arr[ii]%2 == 1:
+                if arr[ii+1]%2 == 1:
+                    if arr[ii+2]%2 == 1:
+                        return True
 
-        for ii in range(ll):
-            pass
-
-        return result
+        return False
 
 
 if __name__ == "__main__":
