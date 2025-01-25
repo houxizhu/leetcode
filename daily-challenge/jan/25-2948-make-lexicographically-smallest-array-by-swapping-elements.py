7f7e41793f2ddb68c3d6139c0b4c8b9f7b836d8e
Xizhu Hou <houxizhu@gmail.com>
2025-01-25 22:29:09 +1100

    leetcode,daily-challenge/jan/25-2948-make-lexicographically-smallest-array-by-swapping-elements.py

---
+++ daily-challenge/jan/25-2948-make-lexicographically-smallest-array-by-swapping-elements.py
@@ -0,0 +1,88 @@
+"""
+2948. Make Lexicographically Smallest Array by Swapping Elements
+Medium
+Topics
+Companies
+Hint
+You are given a 0-indexed array of positive integers nums and a positive integer limit.
+
+In one operation, you can choose any two indices i and j and swap nums[i] and nums[j] if |nums[i] - nums[j]| <= limit.
+
+Return the lexicographically smallest array that can be obtained by performing the operation any number of times.
+
+An array a is lexicographically smaller than an array b if in the first position where a and b differ, array a has an element that is less than the corresponding element in b. For example, the array [2,10,3] is lexicographically smaller than the array [10,2,3] because they differ at index 0 and 2 < 10.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,3,9,8], limit = 2
+Output: [1,3,5,8,9]
+Explanation: Apply the operation 2 times:
+- Swap nums[1] with nums[2]. The array becomes [1,3,5,9,8]
+- Swap nums[3] with nums[4]. The array becomes [1,3,5,8,9]
+We cannot obtain a lexicographically smaller array by applying any more operations.
+Note that it may be possible to get the same result by doing different operations.
+Example 2:
+
+Input: nums = [1,7,6,18,2,1], limit = 3
+Output: [1,6,7,18,1,2]
+Explanation: Apply the operation 3 times:
+- Swap nums[1] with nums[2]. The array becomes [1,6,7,18,2,1]
+- Swap nums[0] with nums[4]. The array becomes [2,6,7,18,1,1]
+- Swap nums[0] with nums[5]. The array becomes [1,6,7,18,1,2]
+We cannot obtain a lexicographically smaller array by applying any more operations.
+Example 3:
+
+Input: nums = [1,7,28,19,10], limit = 3
+Output: [1,7,28,19,10]
+Explanation: [1,7,28,19,10] is the lexicographically smallest array we can obtain because we cannot apply the operation on any two indices.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= limit <= 109
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int], limit: int) -> List[int]:
+        nums.append(max(nums)+limit+2)
+        ll = len(nums)
+        iii_nums = [[nums[ii],ii,0] for ii in range(ll)]
+        iii_nums.sort()
+        for ii in range(ll):
+            iii_nums[ii][2] = ii
+
+        result = [0]*(ll-1)
+        count = 1
+        for ii in range(1,ll):
+            if iii_nums[ii][0] - iii_nums[ii-1][0] > limit:
+                sublist = sorted(iii_nums[ii-count:ii], key=lambda x:x[1])
+                #print(ii,count,iii_nums[ii-count:ii],sublist)
+                for jj in range(ii-count, ii):
+                    iii2 = sublist[jj-(ii-count)][1]
+                    result[iii2] = iii_nums[jj][0]
+                count = 1
+            else:
+                count += 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
