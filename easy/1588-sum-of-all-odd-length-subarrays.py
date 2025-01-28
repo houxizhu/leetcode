429dea747325b9c44f5e713d021b63313ad47eb1
Xizhu Hou <houxizhu@gmail.com>
2025-01-29 00:24:50 +1100

    leetcode,easy/1588-sum-of-all-odd-length-subarrays.py

---
+++ easy/1588-sum-of-all-odd-length-subarrays.py
@@ -0,0 +1,84 @@
+"""
+1588. Sum of All Odd Length Subarrays
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array of positive integers arr, return the sum of all possible odd-length subarrays of arr.
+
+A subarray is a contiguous subsequence of the array.
+
+ 
+
+Example 1:
+
+Input: arr = [1,4,2,5,3]
+Output: 58
+Explanation: The odd-length subarrays of arr and their sums are:
+[1] = 1
+[4] = 4
+[2] = 2
+[5] = 5
+[3] = 3
+[1,4,2] = 7
+[4,2,5] = 11
+[2,5,3] = 10
+[1,4,2,5,3] = 15
+If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
+Example 2:
+
+Input: arr = [1,2]
+Output: 3
+Explanation: There are only 2 subarrays of odd length, [1] and [2]. Their sum is 3.
+Example 3:
+
+Input: arr = [10,11,12]
+Output: 66
+ 
+
+Constraints:
+
+1 <= arr.length <= 100
+1 <= arr[i] <= 1000
+ 
+
+Follow up:
+
+Could you solve this problem in O(n) time complexity?
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, arr: List[int]) -> int:
+        result = 0
+        ll = len(arr)
+        for ii in range(ll+1):
+            if ii%2 == 0:
+                continue
+            sum_i = sum(arr[:ii])
+            result += sum_i
+            for jj in range(1,ll-ii+1):
+                sum_i -= arr[jj-1]
+                sum_i += arr[jj+ii-1]
+                #print(sum_i)
+                result += sum_i
+            #print(ii,result,sum_i)
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
