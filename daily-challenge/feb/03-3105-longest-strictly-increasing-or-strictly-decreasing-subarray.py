295211527e0cdaa354466d76607588e86707a8bb
Xizhu Hou <houxizhu@gmail.com>
2025-02-03 12:10:06 +1100

    leetcode,daily-challenge/feb/03-3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py

---
+++ daily-challenge/feb/03-3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -0,0 +1,104 @@
+"""
+3105. Longest Strictly Increasing or Strictly Decreasing Subarray
+Solved
+Easy
+Topics
+Companies
+You are given an array of integers nums. Return the length of the longest 
+subarray
+ of nums which is either 
+strictly increasing
+ or 
+strictly decreasing
+.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,3,3,2]
+
+Output: 2
+
+Explanation:
+
+The strictly increasing subarrays of nums are [1], [2], [3], [3], [4], and [1,4].
+
+The strictly decreasing subarrays of nums are [1], [2], [3], [3], [4], [3,2], and [4,3].
+
+Hence, we return 2.
+
+Example 2:
+
+Input: nums = [3,3,3,3]
+
+Output: 1
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [3], [3], and [3].
+
+The strictly decreasing subarrays of nums are [3], [3], [3], and [3].
+
+Hence, we return 1.
+
+Example 3:
+
+Input: nums = [3,2,1]
+
+Output: 3
+
+Explanation:
+
+The strictly increasing subarrays of nums are [3], [2], and [1].
+
+The strictly decreasing subarrays of nums are [3], [2], [1], [3,2], [2,1], and [3,2,1].
+
+Hence, we return 3.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+1 <= nums[i] <= 50
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        result = 1
+        ll = len(nums)
+        increasing = 1
+        decreasing = 1
+        for ii in range(1,ll):
+            if nums[ii] == nums[ii-1]:
+                result = max(result, increasing, decreasing)
+                increasing = 1
+                decreasing = 1
+            elif nums[ii] > nums[ii-1]:
+                increasing += 1
+                result = max(result, decreasing)
+                decreasing = 1
+            elif nums[ii] < nums[ii-1]:
+                decreasing += 1
+                result = max(result, increasing)
+                increasing = 1
+        result = max(result, increasing, decreasing)
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
