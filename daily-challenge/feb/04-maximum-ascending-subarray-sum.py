89a21ec7b003ebac865284db472547b15ec6506d
Xizhu Hou <houxizhu@gmail.com>
2025-02-04 11:08:35 +1100

    leetcode,daily-challenge/feb/04-maximum-ascending-subarray-sum.py

---
+++ daily-challenge/feb/04-maximum-ascending-subarray-sum.py
@@ -0,0 +1,67 @@
+"""
+1800. Maximum Ascending Subarray Sum
+Easy
+Topics
+Companies
+Hint
+Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+
+A subarray is defined as a contiguous sequence of numbers in an array.
+
+A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi  < numsi+1. Note that a subarray of size 1 is ascending.
+
+ 
+
+Example 1:
+
+Input: nums = [10,20,30,5,10,50]
+Output: 65
+Explanation: [5,10,50] is the ascending subarray with the maximum sum of 65.
+Example 2:
+
+Input: nums = [10,20,30,40,50]
+Output: 150
+Explanation: [10,20,30,40,50] is the ascending subarray with the maximum sum of 150.
+Example 3:
+
+Input: nums = [12,17,15,13,10,11,12]
+Output: 33
+Explanation: [10,11,12] is the ascending subarray with the maximum sum of 33.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, nums: List[int]) -> int:
+        nums.append(min(nums)-1)
+        ll = len(nums)
+        result = nums[0]
+        ascending = nums[0]
+        for ii in range(1, ll):
+            if nums[ii] > nums[ii-1]:
+                ascending += nums[ii]
+            else:
+                result = max(result, ascending)
+                ascending = nums[ii]
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
