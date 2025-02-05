8eae89e13c9d226a9c4ce21230d260d895718ce5
Xizhu Hou <houxizhu@gmail.com>
2025-02-06 01:15:16 +1100

    medium/3-longest-substring-without-repeating-characters.py

---
+++ medium/3-longest-substring-without-repeating-characters.py
@@ -0,0 +1,69 @@
+"""
+3. Longest Substring Without Repeating Characters
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str) -> int:
+        ll = len(s)
+        result = 0
+        window = 0
+        start = 0
+        for ii in range(ll):
+            if s[ii] in s[start:ii]:
+                result = max(result,ii-start)
+                while s[start] != s[ii]:
+                    start += 1
+                start += 1
+        result = max(result,ll-start)
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
