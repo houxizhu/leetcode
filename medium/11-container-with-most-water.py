145bf73772246a9aaae722e890059298d8e57ae5
Xizhu Hou <houxizhu@gmail.com>
2025-02-08 00:27:15 +1100

    leetcode,medium/11-container-with-most-water.py

---
+++ medium/11-container-with-most-water.py
@@ -0,0 +1,69 @@
+"""
+11. Container With Most Water
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, height: List[int]) -> int:
+        n = len(height)
+        result = min(height[0], height[-1])*(n-1)
+        left = 0
+        right = n-1
+        while left < right:
+            volume = min(height[left], height[right])*(right-left)
+            result = max(result, volume)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
