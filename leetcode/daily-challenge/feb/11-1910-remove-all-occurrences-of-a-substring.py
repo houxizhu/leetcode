af80359116623d9cfd422a431bf3dea1070303a2
houxizhu <houxizhu@gmail.com>
2025-02-11 12:42:56 +1100

    leetcode,leetcode/daily-challenge/feb/11-1910-remove-all-occurrences-of-a-substring.py

---
+++ leetcode/daily-challenge/feb/11-1910-remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,71 @@
+"""
+1910. Remove All Occurrences of a Substring
+
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+
+A substring is a contiguous sequence of characters in a string.
+
+ 
+
+Example 1:
+
+Input: s = "daabcbaabcbc", part = "abc"
+Output: "dab"
+Explanation: The following operations are done:
+- s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+- s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+- s = "dababc", remove "abc" starting at index 3, so s = "dab".
+Now s has no occurrences of "abc".
+Example 2:
+
+Input: s = "axxxxyyyyb", part = "xy"
+Output: "ab"
+Explanation: The following operations are done:
+- s = "axxxxyyyyb", remove "xy" starting at index 4 so s = "axxxyyyb".
+- s = "axxxyyyb", remove "xy" starting at index 3 so s = "axxyyb".
+- s = "axxyyb", remove "xy" starting at index 2 so s = "axyb".
+- s = "axyb", remove "xy" starting at index 1 so s = "ab".
+Now s has no occurrences of "xy".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+1 <= part.length <= 1000
+s​​​​​​ and part consists of lowercase English letters.
+"""
+
+import string
+import heapq
+from typing import List
+from collections import defaultdict
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def leetcode(self, s: str, part: str) -> str:
+        ll = len(s)
+        llp = len(part)
+        result = ""
+        count = 0
+        for each in s:
+            result += each
+            count += 1
+            if count >= llp and result[(count-llp):] == part:
+                result = result[0:(count-llp)]
+                count -= llp
+
+        return result
+
+
+if __name__ == "__main__":
+    app = Solution()
+    a = [4,5,0,2,3,1]
+    print(app.leetcode(a))
